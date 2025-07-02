24b390ff7d8a18869baf80615c02081d5470bd65
Shamil Akhmadov <shamil@Shamils-MacBook-Air.local>
2025-07-02 19:19:20 +0300

    Added new_task/resolve 2629. Function Composition

--- LeetCode_Practice.js
+++ LeetCode_Practice.js
@@ -90,3 +90,40 @@ let resultOfMethods = createCounter(14);
 console.log(resultOfMethods.increment());
 console.log(resultOfMethods.decrement());
 console.log(resultOfMethods.reset());
+
+
+
+/*2629. Function Composition: Given an array of functions [f1, f2, f3, ..., fn], return a new function fn 
+that is the function composition of the array of functions.
+
+The function composition of [f(x), g(x), h(x)] is fn(x) = f(g(h(x))).
+The function composition of an empty list of functions is the identity function f(x) = x.
+
+You may assume each function in the array accepts one integer as input and returns one integer as output.*/
+
+function f1(x) {
+    return 2 + x;
+}
+
+function f2(x) {
+    return 2 - x;
+}
+
+function f3(x) {
+    return 2 * x;
+}
+
+let arr = [f1, f2, f3];
+
+function fn() {
+    function composition(x){
+        let result = x;
+        for(let i = arr.length - 1; i >= 0; i--){
+            result = arr[i](result);
+        }
+        return result;
+    }
+    return composition;
+}
+let newFun = fn();
+console.log(newFun(5));
